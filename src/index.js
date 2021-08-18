9a7efebc9d7919ff657d6a426eaa0d86917b5ce8
dony <trantrungisk5@gmail.com>
2021-08-18 18:04:36 +0700

    [Init] source

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { StyleReset } from "atomize";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+// 2. Provide the engine to the app
+// debug engine needs inlined source maps
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <StyleReset />
+      <App />
+    </StyletronProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
